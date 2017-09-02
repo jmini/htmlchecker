e572372081c160167d131acdc416cac30a2b47ba
Jeremie Bresson <dev@jmini.fr>
2017-09-02 08:53:53 +0200

    WIP

---
+++ htmlchecker-cli/src/main/scripts/htmlchecker.cmd
@@ -0,0 +1,1 @@
+java -cp lib/* fr.jmini.charchecker.cli.Main %*
---
+++ htmlchecker-cli/src/test/java/fr/jmini/charchecker/cli/MainTest.java
@@ -0,0 +1,6 @@
+package fr.jmini.charchecker.cli;
+
+public class MainTest {
+
+//XXX compare the help output with the help output.
+}
