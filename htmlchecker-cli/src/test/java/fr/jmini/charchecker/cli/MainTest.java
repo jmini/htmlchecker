d205bcde0ef7400b01130efbe219f6de42e36fc3
Jeremie Bresson <dev@jmini.fr>
2017-09-10 07:57:44 +0200

    WIP

--- htmlchecker-cli/src/test/java/fr/jmini/charchecker/cli/MainTest.java
+++ htmlchecker-cli/src/test/java/fr/jmini/charchecker/cli/MainTest.java
@@ -1,6 +1,26 @@
 package fr.jmini.charchecker.cli;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+
+import com.google.common.base.Charsets;
+import com.google.common.io.Resources;
+import com.selesse.jxlint.cli.CommandLineOptions;
+
+import fr.jmini.htmlchecker.settings.HtmlCheckerProgramSettings;
+
 public class MainTest {
 
-//XXX compare the help output with the help output.
+  @Test
+  public void testHelpMessage() throws Exception {
+    String helpMessage = CommandLineOptions.getHelpMessage(new HtmlCheckerProgramSettings());
+
+    String expected = Resources.toString(Resources.getResource("htmlchecker-help.txt"), Charsets.UTF_8);
+    assertThat(normalizeLineEnds(helpMessage)).isEqualTo(normalizeLineEnds(expected));
+  }
+
+  private static String normalizeLineEnds(String s) {
+    return s.replace("\r\n", "\n").replace('\r', '\n');
+  }
 }
