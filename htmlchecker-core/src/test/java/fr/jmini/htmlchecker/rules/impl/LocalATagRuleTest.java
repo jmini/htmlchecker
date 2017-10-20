5be577dd1add1eff9d4dc183ca0ab4c3b167bba2
Jeremie Bresson <dev@jmini.fr>
2017-10-20 05:02:26 +0200

    Add additional tests for <a> tag

--- htmlchecker-core/src/test/java/fr/jmini/htmlchecker/rules/impl/LocalATagRuleTest.java
+++ htmlchecker-core/src/test/java/fr/jmini/htmlchecker/rules/impl/LocalATagRuleTest.java
@@ -12,6 +12,7 @@ import com.google.common.io.Resources;
 import com.selesse.jxlint.model.rules.LintError;
 
 public class LocalATagRuleTest extends AbstractLocalRuleTest<LocalATagRule> {
+  protected static final String OTHER_PAGE_NAME = "other-a.html";
 
   @Test
   public void testGetLintErrors() throws Exception {
@@ -71,6 +72,27 @@ public class LocalATagRuleTest extends AbstractLocalRuleTest<LocalATagRule> {
     File file = new File(Resources.getResource("root-a.html").getPath());
     List<LintError> lintErrors = rule.getLintErrors(file);
     assertThat(lintErrors).hasSize(0);
+  }
+
+  @Test
+  public void testGetLintErrorsWithOther() throws Exception {
+    setupRule();
+    File file = new File(Resources.getResource(OTHER_PAGE_NAME).getPath());
+    List<LintError> lintErrors = rule.getLintErrors(file);
+    assertThat(lintErrors).hasSize(2);
+
+    for (LintError lintError : lintErrors) {
+      switch (lintError.getLineNumber()) {
+        case 12:
+          assertLintError(lintError, OTHER_PAGE_NAME, 12, "Anchor 'nonsense' referenced in the 'href' attribute in the 'a' tag is missing in file  'dir/file.txt' (relative to 'other-a.html')");
+          break;
+        case 13:
+          assertLintError(lintError, OTHER_PAGE_NAME, 13, "Anchor 'nonsense' referenced in the 'href' attribute in the 'a' tag is missing in file  'dir/pict.png' (relative to 'other-a.html')");
+          break;
+        default:
+          fail("Did not expect LintError " + lintError);
+      }
+    }
   }
 
   @Override
