a4b0d480cdf4784ff258ae591719e6b29aec8f41
Jeremie Bresson <jeremie.bresson@unblu.com>
2022-02-21 22:54:48 +0100

    Fix test after version bump

--- htmlchecker-core/src/main/java/fr/jmini/htmlchecker/settings/HtmlCheckerProgramSettings.java
+++ htmlchecker-core/src/main/java/fr/jmini/htmlchecker/settings/HtmlCheckerProgramSettings.java
@@ -7,7 +7,7 @@ import com.selesse.jxlint.settings.ProgramSettings;
 public class HtmlCheckerProgramSettings implements ProgramSettings {
   @Override
   public String getProgramVersion() {
-    return "1.2.2";
+    return "1.2.3";
   }
 
   @Override
